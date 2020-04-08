7fe96a8e611324c4ab6fbe158bf1830b7a9c8000
Sean Ewing <sewing3@uoregon.edu>
2020-04-08 13:03:03 -0700

    Add arrMax to functions.js

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -9,3 +9,16 @@ function arrAvg(array) {
   var avg = sum / numCount;
   return avg;
 }
+
+function arrMax(array) {
+  return Math.max(array);
+}
+
+let temps = [31.9, 31.3, 42.4, 42.4, 60.8];
+let sum = 0,
+  average;
+for (var i = 0; i < temps.length; i++) {
+  sum += temps[i];
+}
+average = sum / temps.length;
+console.log(average);
